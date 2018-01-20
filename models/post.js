63aa655dbc97d04ef5046f8a63d64d6a07c1e475
Ismail BH <benhamouda.ismail@gmail.com>
2018-01-20 22:16:40 +0100

    add mongoose schema post

---
+++ models/post.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+mongoose.Promise = Promise;
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    uid: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    }
+  },
+  { collection: "post", timestamps: true }
+);
+
+module.exports = mongoose.model("post", postSchema);
