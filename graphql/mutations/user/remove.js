2493e7e89609516bf28c0487479258dbde1d9dc0
Ismail BH <benhamouda.ismail@gmail.com>
2018-01-21 00:05:55 +0100

    graphql  mutation user

---
+++ graphql/mutations/user/remove.js
@@ -0,0 +1,25 @@
+import {
+	GraphQLNonNull,
+	GraphQLID
+} from 'graphql'
+
+import { userType, userInputType } from '../../types/user';
+import UserModel from '../../../models/user';
+
+
+export default {
+	type: userType,
+	args: {
+		id: {
+			name: 'id',
+			type: new GraphQLNonNull(GraphQLID)
+		}
+	},
+	resolve(root, params) {
+		const removeduser = UserModel.findByIdAndRemove(params.id).exec();
+		if (!removeduser) {
+			throw new Error('Error removing user')
+		}
+		return removeduser;
+	}
+}
---
+++ graphql/types/index.js
@@ -0,0 +1,9 @@
+import userType from './user';
+import postType from './post';
+import compteType from './compte';
+
+export default {
+  userType,
+  postType, 
+  compteType
+}
